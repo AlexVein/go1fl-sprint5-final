3a69f7fe060a8b947dd9f2aea0a89699b944e914
Aleksei Velichkin <mail@avelichkin.ru>
2025-05-03 17:25:06 +0300

    feat(daysteps): implement the DaySteps.Parse() method

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,9 +1,19 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+)
+
+var (
+	ErrInvalidArgumentsCount = errors.New("invalid arguments count")
+	ErrInvalidFormat         = errors.New("invalid format")
+	ErrUnknownTrainingType   = errors.New("неизвестный тип тренировки")
 )
 
 type DaySteps struct {
@@ -13,7 +23,22 @@ type DaySteps struct {
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	data := strings.Split(datastring, ",")
+	if len(data) != 2 {
+		return fmt.Errorf("%w: %s", ErrInvalidArgumentsCount, datastring)
+	}
+
+	ds.Steps, err = strconv.Atoi(data[0])
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidFormat, data[0])
+	}
+
+	ds.Duration, err = time.ParseDuration(data[1])
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrInvalidFormat, data[1])
+	}
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
